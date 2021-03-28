4cc345b00661209720957c5e5e844deda5e29b87
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-03-28 13:46:23 +0100

    Delete user_id attribute and change relationships

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,27 +1,24 @@
 from backend.extensions import db
 from .task import Task
 from marshmallow import Schema, fields
-from datetime import datetime
+from datetime import date
 
 
 class User(db.Model):
     __tablename__ = 'users'
 
-    user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), primary_key=True, nullable=False)
     email = db.Column(db.String(100), nullable=False)
-    registered = db.Column(db.DateTime, nullable=False,
-                             default=datetime.utcnow)
+    registered = db.Column(db.DateTime, default=date.today())
 
-    tasks = db.relationship("Tasks", backref="tasks", lazy=True)
+    tasks = db.relationship("Task", backref="tasks", lazy=True)
 
     def __repr__(self):
-        return "<User(user_id='%s', username='%s', email='%s')>" \
-                % (self.user_id, self.username, self.email)
+        return "<User(username='%s', email='%s')>" \
+                % (self.username, self.email)
 
 
 class UserSchema(Schema):
-    user_id = fields.Integer(dump_only=True)
     username = fields.String()
     email = fields.String()
     registered = fields.DateTime(dump_only=True)
