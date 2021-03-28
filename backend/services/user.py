721cb4468ce625dfb38714a15e1d8f1e07f5bdf6
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-03-28 14:54:08 +0100

    Add methods for getting user, checking for adding user
    Add methods for checking if the user has been already used and if the email has already been taken.

--- backend/services/user.py
+++ backend/services/user.py
@@ -1,12 +1,19 @@
 from backend.models.user import user_schema, User
 from backend.extensions import db
 from datetime import datetime
-from backend.services.task import get_user
 
 
 def create_user(data_json):
     if not data_json:
         return {"status": "failed", "message": "No data provided"}
+
+    if is_registered(data_json['username']):
+        return {"status": "fail",
+                "message": "Username has been already taken"}
+
+    if is_email_registered(data_json['email']):
+        return {"status": "fail",
+                "message": "Email is used in this service."}
 
     data = user_schema.load(data_json)
     new_user = User(**data)
@@ -16,3 +23,36 @@ def create_user(data_json):
 
     return {"status": "success", "data": user_schema.dump(new_user)}
 
+
+def get_user(data_json):
+    if not data_json:
+        return {"status": "fail", "message": "No data provided."}
+    else:
+        if is_registered(data_json['username']):
+            registered_user = user_schema.dump(data_json)
+            return {"status": "success", "user": registered_user}
+        else:
+            return {"status": "fail",
+                    "message": f"User {data_json['username']} has not been registered yet."}
+
+
+def is_registered(username):
+    try:
+        found_user = User.query.get(username)
+        if found_user is None:
+            return False
+        else:
+            return True
+    except ValueError as e:
+        return {"success": False, "message": "User with provided username not found."}
+
+
+def is_email_registered(email):
+    try:
+        found_email = User.query.filter_by(email=email).first()
+        if found_email is None:
+            return False
+        else:
+            return True
+    except ValueError as e:
+        return {"success": False, "message": "User with provided email not found."}
