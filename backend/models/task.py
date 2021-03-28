d582120a8d42592a639362e613427ddadefee525
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-03-28 10:25:25 +0100

    Add marshmallow api for serializing and deserializing data
    Added Task schema for serializing and deserializing dtaa

--- backend/models/task.py
+++ backend/models/task.py
@@ -1,11 +1,12 @@
 from backend.extensions import db
 from datetime import datetime
+from marshmallow import Schema, fields
 
 
 class Task(db.Model):
     __tablename__ = 'tasks'
 
-    id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String, nullable=False)
     description = db.Column(db.String)
     is_active = db.Column(db.Boolean, default=True)
@@ -16,5 +17,17 @@ class Task(db.Model):
                         nullable=False)
 
     def __repr__(self):
-        return "<Task(id='%s', task_name='%s', description='%s', is_active='%s')>" \
+        return "<Task(task_id='%s', task_name='%s', description='%s', is_active='%s')>" \
                % (self.id, self.task_name, self.description, self.is_active)
+
+
+class TaskSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    task_name = fields.String()
+    description = fields.String()
+    is_active = fields.Boolean()
+    date_created = fields.DateTime(dump_only=True)
+
+
+task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
