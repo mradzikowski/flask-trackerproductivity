5686ad3650237d33076191eef735bea28dd788cb
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-03-28 10:25:47 +0100

    Add marshmallow api for serializing and deserializing data
    Added User schema for serializing and deserializing data. Added attribute for storing date of registered

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,5 +1,7 @@
 from backend.extensions import db
 from .task import Task
+from marshmallow import Schema, fields
+from datetime import datetime
 
 
 class User(db.Model):
@@ -8,9 +10,22 @@ class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), nullable=False)
+    registered = db.Column(db.DateTime, nullable=False,
+                             default=datetime.utcnow)
 
     tasks = db.relationship("Tasks", backref="tasks", lazy=True)
 
     def __repr__(self):
         return "<User(user_id='%s', username='%s', email='%s')>" \
                 % (self.user_id, self.username, self.email)
+
+
+class UserSchema(Schema):
+    user_id = fields.Integer(dump_only=True)
+    username = fields.String()
+    email = fields.String()
+    registered = fields.DateTime(dump_only=True)
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
