6317a44aa6d62e623b620eaa90429d25c62cac5b
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-03-28 10:02:06 +0100

    Add file for database migration

---
+++ manage.py
@@ -0,0 +1,16 @@
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from backend import create_app
+from backend.extensions import db
+
+app = create_app()
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
