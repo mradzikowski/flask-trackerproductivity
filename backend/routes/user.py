80efc4490a4143eb898e330533215662d08627f1
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-03-28 21:46:12 +0100

    Add routes for deleting user and getting user

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -8,7 +8,7 @@ def index():
     return render_template("index.html")
 
 
-@bp.route('/register', methods=['POST'])
+@bp.route('/user', methods=['POST'])
 def create_user():
     try:
         data_json = request.json
@@ -16,5 +16,25 @@ def create_user():
         return jsonify(body)
     except ValueError as e:
         return e
+
+
+@bp.route('/user', methods=['GET'])
+def get_user():
+    try:
+        data_json = request.json
+        return jsonify(user_services.get_user(data_json))
+    except ValueError as e:
+        return {"status": "fail", "message": e}
+
+
+@bp.route('/user', methods=['DELETE'])
+def delete_user():
+    try:
+        data_json = request.json
+        return jsonify(user_services.delete_user(data_json))
+    except ValueError as e:
+        return {"status": "fail", "message": e}
+
+
 
 
