aa4839f5423cebb538f6b19cb127534e4312aae6
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-03-28 10:00:24 +0100

    Add configuration to creating app

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,19 @@
 from flask import Flask, request
+from .extensions import db
+import os
+from .models.task import Task
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config.from_object(os.environ['APP_SETTINGS'])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    from .models.task import Task
+    from .models.user import User
 
     @app.route('/')
     def index():
