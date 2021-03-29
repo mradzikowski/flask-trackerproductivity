eabe5ae45859e7a7cb3d9c21425b131eacfc4d2a
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-03-29 12:58:39 +0100

    Change import to import task routes

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -3,4 +3,4 @@ from flask import Blueprint
 bp = Blueprint('routes', __name__, template_folder='templates')
 
 from backend.routes.user import *
-from backend.routes.user import *
+from backend.routes.task import *
