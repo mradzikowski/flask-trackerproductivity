c88531db2a7245150b9cb210eb79ff56df7d0ce7
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-03-29 13:02:19 +0100

    Add return objects as response status hhtp codes

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,28 +1,24 @@
-from flask import request, jsonify, render_template
+from flask import request, jsonify
 from . import bp
 import backend.services.user as user_services
-
-
-@bp.route('/')
-def index():
-    return render_template("index.html")
 
 
 @bp.route('/user', methods=['POST'])
 def create_user():
     try:
         data_json = request.json
-        body = user_services.create_user(data_json)
-        return jsonify(body)
+        body, status = user_services.create_user(data_json)
+        return jsonify(body), status
     except ValueError as e:
-        return e
+        return {"status": "fail", "message": e}
 
 
 @bp.route('/user', methods=['GET'])
 def get_user():
     try:
         data_json = request.json
-        return jsonify(user_services.get_user(data_json))
+        body, status = user_services.get_user(data_json)
+        return jsonify(body), status
     except ValueError as e:
         return {"status": "fail", "message": e}
 
@@ -31,7 +27,8 @@ def get_user():
 def delete_user():
     try:
         data_json = request.json
-        return jsonify(user_services.delete_user(data_json))
+        body, status = user_services.delete_user(data_json)
+        return jsonify(body)
     except ValueError as e:
         return {"status": "fail", "message": e}
 
