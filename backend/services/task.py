4180ff17154c112b8888bea52d114f6dd3734178
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-03-29 13:01:13 +0100

    Add create method for creating and saving task object
    Method also updates user to add connect task in regards to relationship

--- backend/services/task.py
+++ backend/services/task.py
@@ -1,12 +1,41 @@
-from backend.models.task import TaskSchema, Task
+from backend.models.task import task_schema, Task
 from backend.models.user import User
+from datetime import datetime
+from backend.extensions import db
 
 
-def get_user(username):
+def get_user(data_json):
+    if not data_json:
+        return {"status": "fail", "message": "No data provided."}
+    if not data_json['username']:
+        return {"status": "fail", "message": "No username provided."}
+    username = data_json['username']
+    return User.query.filter_by(username=username).first()
+
+
+def create_task(data_json):
+    if not data_json:
+        return {"status": "fail", "message": "No data provided."}
+    condition_for_task = (data_json['task_id'] and
+                          data_json['task_name'] and data_json['username'])
+    if not condition_for_task:
+        return {"status": "fail", "message": "Data does not meet condition for user."}
     try:
-        found_user = User.objects.filter(username=username).first()
-        return found_user
-    except Exception as e:
-        return {"success": False, "message": "User with provided username not found."}
+        data = task_schema.load(data_json)
+        new_task = Task(**data)
+        new_task.is_active = True
+        new_task.date_created = datetime.now()
+
+        user = get_user(data_json)
+        user.tasks.append(new_task)
+        db.session.add(user)
+        db.session.add(new_task)
+        db.session.commit()
+
+        return_json = task_schema.dump(data_json)
+
+        return {"status": "success", "message": return_json}, 201
+    except ValueError as e:
+        return {"status": "fail", "message": "Error while creating object"}
 
 
