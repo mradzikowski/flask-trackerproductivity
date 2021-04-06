28d0f1f28fb959cb33d1c4311d0cb2066c48f7d0
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-04-06 23:47:34 +0100

    Change routes to take arguments and add checks for requests
    Change approach for having routes. Added arguments passed in urls, deleted try/catch blocks not to duplicate oif from services.

--- backend/routes/task.py
+++ backend/routes/task.py
@@ -5,65 +5,52 @@ import backend.services.task as task_service
 from . import bp
 
 
-@bp.route('/task', methods=['POST'])
+@bp.route('/task', methods=['GET', 'POST'])
 def create_task():
-    try:
+    if request.method == "GET":
+        body, status = task_service.get_all_tasks()
+    elif request.method == "POST":
         data_json = request.json
         body, status = task_service.create_task(data_json)
-        return jsonify(body), status
-    except ValueError as e:
-        return {"success": False, "message": "Error while creating object."}
+    else:
+        body, status = None, 405
+    return jsonify(body), status
 
 
-@bp.route('/task', methods=['GET'])
-def get_task():
-    try:
-        data_json = request.json
-        body, status = task_service.get_task(data_json)
-        return jsonify(body), status
-    except ValueError as e:
-        return {"success": False,
-                "message": "Error while retrieving task from database."}
+@bp.route('/task/<int:pk>', methods=['GET', 'DELETE'])
+def delete_task(pk):
+    if request.method == "GET":
+        body, status = task_service.get_task(pk)
+    elif request.method == "DELETE":
+        body, status = task_service.delete_task(pk)
+    else:
+        body, status = None, 405
+
+    return jsonify(body), status
 
 
-@bp.route('/task', methods=['DELETE'])
-def delete_task():
-    try:
-        data_json = request.json
-        body, status = task_service.delete_task(data_json)
-        return jsonify(body), status
-    except ValueError as e:
-        return {"success": False,
-                "message": "Error while deleting task object from database."}
-
-
-@bp.route('/task/finish', methods=['PATCH'])
-def finish_task():
-    try:
-        data_json = request.json
-        body, status = task_service.finish_task(data_json)
-        return jsonify(body), status
-    except ValueError as e:
-        return {"success": False,
-                "message": "Error while finishing the task"}
+@bp.route('/task/<int:pk>/finish', methods=['PATCH'])
+def finish_task(pk):
+    if request.method == "PATCH":
+        body, status = task_service.finish_task(pk)
+    else:
+        body, status = None, 405
+    return jsonify(body), status
 
 
 @bp.route('/task/get/all', methods=['GET'])
 def get_all_tasks():
-    try:
+    if request.method == "GET":
         body, status = task_service.get_all_tasks()
-        return jsonify(body), status
-    except ValueError as e:
-        return {"success": False,
-                "message": "Error while retrieving all tasks"}
+    else:
+        body, status = None, 405
+    return jsonify(body), status
 
 
-@bp.route('/task/user/week', methods=['GET'])
-def get_tasks_from_last_week():
-    try:
-        data = request.json
-        body, status = task_service.get_tasks_from_last_week(data)
-        return jsonify(body), status
-    except ValueError as e:
-        return {"success": False,
-                "message": "Error while retrieving recent tasks"}
+@bp.route('/task/user/<pk>/week', methods=['GET'])
+def get_tasks_from_last_week(pk):
+    if request.method == "GET":
+        body, status = task_service.get_tasks_from_last_week(pk)
+    else:
+        body, status = None, 405
+    return jsonify(body), status
