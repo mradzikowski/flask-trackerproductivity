c49cf0bf95870df3d6d25258262672019a786ea6
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-04-06 22:10:36 +0100

    Change urls to take arguments passed
    Deleted try/catch blocks due to having them in services. Checks for method requests.

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -5,75 +5,66 @@ import backend.services.user as user_services
 from . import bp
 
 
-@bp.route('/user', methods=['POST'])
+@bp.route('/user', methods=['POST', 'GET'])
 def create_user():
-    try:
+    if request.method == "POST":
         data_json = request.json
         body, status = user_services.create_user(data_json)
-        return jsonify(body), status
-    except ValueError as e:
-        return {"success": False, "message": e}
+    elif request.method == "GET":
+        body, status = user_services.get_all_users()
+    else:
+        body, status = None, 405
+
+    return jsonify(body), status
 
 
-@bp.route('/user', methods=['GET'])
-def get_user():
-    try:
-        data_json = request.json
-        body, status = user_services.get_user(data_json)
-        return jsonify(body), status
-    except ValueError as e:
-        return {"success": False, "message": e}
+@bp.route('/user/<pk>', methods=['GET', 'DELETE'])
+def get_user(pk):
+    if request.method == "GET":
+        body, status = user_services.get_user(pk)
+    elif request.method == "DELETE":
+        body, status = user_services.delete_user(pk)
+    else:
+        body, status = None, 405
+
+    return jsonify(body), status
 
 
-@bp.route('/user', methods=['DELETE'])
-def delete_user():
-    try:
-        data_json = request.json
-        body, status = user_services.delete_user(data_json)
-        return jsonify(body), status
-    except ValueError as e:
-        return {"success": False, "message": e}
+@bp.route('/user/<pk>/tasks', methods=['GET'])
+def get_all_tasks_for_user(pk):
+    if request.method == "GET":
+        active = request.args.get('active')
+
+        if active is None:
+            body, status = user_services.get_all_tasks_for_user(pk)
+        if active.upper() == "TRUE":
+            active = True
+        elif active.upper() == "FALSE":
+            active = False
+        else:
+            return {"success": False, "message": "Invalid argument key."}, 400
+
+        body, status = user_services.get_all_active_tasks_for_user(pk, active)
+    else:
+        body, status = None, 405
+    return jsonify(body), status
 
 
-@bp.route('/user/tasks', methods=['GET'])
-def get_all_tasks_for_user():
-    try:
-        data_json = request.json
-        body, status = user_services.get_all_tasks_for_user(data_json)
-        return jsonify(body), status
-    except ValueError as e:
-        return {"success": False,
-                "message": "Error while getting tasks for user"}
-
-
-@bp.route('/user/tasks/active', methods=['GET'])
-def get_all_active_tasks_for_user():
-    try:
-        data_json = request.json
-        body, status = user_services.get_all_active_tasks_for_user(data_json)
-        return jsonify(body), status
-    except ValueError as e:
-        return {"success": False,
-                "message": "Error while getting active tasks for user"}
-
-
-@bp.route('/user/tasks/productivity', methods=['GET'])
-def get_productivity_for_user():
-    try:
-        data_json = request.json
-        body, status = user_services.get_all_tasks_and_calculate_productivity(data_json)
-        return jsonify(body), status
-    except ValueError as e:
-        return {"success": False,
-                "message": "Error while getting productive hours"}
+@bp.route('/user/<pk>/tasks/productivity', methods=['GET'])
+def get_productivity_for_user(pk):
+    if request.method == "GET":
+        body, status = user_services.get_all_tasks_and_calculate_productivity(pk)
+    else:
+        body, status = None, 405
+    return jsonify(body), status
 
 
 @bp.route('/user/get/all', methods=['GET'])
 def get_all_users():
-    try:
+    if request.methdod == "GET":
         body, status = user_services.get_all_users()
-        return jsonify(body), status
-    except ValueError as e:
-        return {"success": False,
-                "message": "Error while retrieving all users"}
+    else:
+        body, status = None, 405
+    return jsonify(body), status
+
 
