029103b80e21b6c64801816fe8dc27585317cb02
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-04-06 23:49:37 +0100

    Change code to use filters for getting objects
    Added arguments passed from url and used them for filtering and retrieving objects from database. Relocated methods to helper.py file. Made code more readable and easy to understand the purpose of the methods.

--- backend/services/task.py
+++ backend/services/task.py
@@ -1,17 +1,12 @@
 from datetime import datetime, timedelta
 
+from sqlalchemy import exc
+
 from backend.extensions import db
 from backend.models.task import Task, task_schema, tasks_schema
 from backend.models.user import User
-
-
-def get_user(data_json):
-    if not data_json:
-        return {"success": False, "message": "No data provided."}
-    if not data_json['username']:
-        return {"success": False, "message": "No username provided."}
-    username = data_json['username']
-    return User.query.filter_by(username=username).first()
+from backend.services.helper import (calculate_duration, get_tasks_last_week,
+                                     get_user)
 
 
 def create_task(data_json):
@@ -39,106 +34,81 @@ def create_task(data_json):
 
         return {"success": True, "message": return_json}, 201
     except KeyError as e:
-        return {"success": False, "message": "Error while creating object"}, 400
-
-
-def get_task(data_json):
-    if not data_json:
-        return {"success": False, "message": "No data provided."}, 400
-    if data_json['task_id']:
-        found_task = Task.query.get(data_json['task_id'])
-        if found_task:
-            registered_task = task_schema.dump(found_task)
-            return {"success": True, "task": registered_task}, 200
-        else:
-            return {"success": False,
-                    "message": "Task has not been registered yet."}, 400
-    else:
         return {"success": False,
-                "message": f"Task - {data_json['task_id']} - "
-                           f"data has not got specified task_id"}, 400
+                "message": "Error while creating object"}, 400
 
 
-def delete_task(data_json):
-    if not data_json:
-        return {"success": False, "message": "No data provided."}, 400
+def get_task(task_id):
     try:
-        if data_json['task_id']:
-            found_task = Task.query.get(data_json['task_id'])
-            if found_task is not None:
-                db.session.delete(found_task)
-                db.session.commit()
-                return {"success": True,
-                        "task": task_schema.dump(found_task),
-                        "message": "Tak has been deleted."}, 200
-            else:
-                return {"success": False,
-                        "message": "No registered task to delete."}, 400
-        else:
-            return {"success": False, "message": "There is no identifier."}
-    except KeyError as e:
+        found_task = Task.query.get(task_id)
+        if found_task is None:
+            return {"success": False, "message": "No task found"}, 422
+
+        task_json = task_schema.dump(found_task)
+        return {"success": True, "data": task_json}, 200
+    except ValueError as e:
         return {"success": False,
-                "message": "Error while deleting and object"}, 400
+                "message": "Error while getting a task"}, 400
 
 
-def finish_task(data_json):
-    if not data_json:
-        return {"success": False, "message": "No data provided."}, 400
+def delete_task(task_id):
+    found_task = User.query.get(task_id)
+    if found_task is None:
+        return {"success": False, "message": "No task found"}, 422
+
+    found_task.delete()
+
     try:
-        if data_json['task_id']:
-            found_task = Task.query.get(data_json['task_id'])
-            if found_task is not None:
-                if found_task.is_active:
-                    found_task.is_active = False
-                    duration = datetime.now() - found_task.date_created
-                    duration = divmod(duration.total_seconds(), 60)
-                    duration_rounded_two_places = round(duration[0] / 60, 2)
-                    found_task.duration = duration_rounded_two_places
-                    db.session.commit()
-                    return {"success": True,
-                            "task": task_schema.dump(found_task)}, 200
-                else:
-                    return {"success": False,
-                            "message": "Task has been already finished"}, 400
-            else:
-                return {"success": False,
-                        "message": "No registered task to delete."}, 400
+        db.session.commit()
+    except exc.SQLAlchemyError as e:
+        return {"success": "False",
+                "message": "Error while committing the session"}, 400
+
+    return {"success": True,
+            "message": "Task " + str(task_id) + " has been deleted"}, 200
+
+
+def finish_task(task_id):
+    try:
+        found_task = Task.query.get(task_id)
+        if found_task is None:
+            return {"success": False, "message": "No task found"}, 422
+
+        if found_task.is_active:
+            found_task = calculate_duration(found_task)
+            db.session.commit()
+            return {"success": True,
+                    "task": task_schema.dump(found_task)}, 200
         else:
-            return {"success": False,
-                    "message": "There is no identifier."}, 400
-    except KeyError as e:
+            return {"success": True,
+                    "message": "Task has been already finished"}, 200
+    except ValueError as e:
         return {"success": False,
-                "message": "Error while deleting and object"}, 400
+                "message": "Error while finishing the task"}
 
 
 def get_all_tasks():
-    data = Task.query.all()
-    data_json = tasks_schema.dump(data)
-    return {"success": True, "data": data_json}, 200
-
-
-def get_tasks_from_last_week(data_json):
-    if not data_json:
-        return {"success": False, "message": "No data provided."}, 400
     try:
-        if data_json['username']:
-            found_user = User.query.get(data_json['username'])
-            if found_user is not None:
-                time_diff = datetime.today() - timedelta(days=7)
-                tasks_user = found_user.tasks
-                tasks_last_week = []
-                for task in tasks_user:
-                    if task.date_created >= time_diff:
-                        tasks_last_week.append(task)
-                return {"success": True,
-                        "message": tasks_schema.dump(tasks_last_week)}, 200
-            else:
-                return {"success": False,
-                        "message": "No registered task to delete."}, 400
-        else:
-            return {"success": False,
-                    "message": f"User - {data_json['username']}"
-                               f" - has not been registered yet."}, 400
+        data = Task.query.all()
+        data_json = tasks_schema.dump(data)
+        return {"success": True, "data": data_json}, 200
     except ValueError as e:
         return {"success": False,
-                "message": "Error while retrieving recent tasks"}, 400
+                "message": "Error while retrieving all tasks"}
+
+
+def get_tasks_from_last_week(username):
+    try:
+        user_found = User.query.get(username)
+
+        if user_found is None:
+            return {"success": False, "message": "No user found"}, 422
+
+        tasks_last_week = get_tasks_last_week(user_found)
+
+        return {"success": True,
+                "tasks": tasks_schema.dump(tasks_last_week)}, 200
+
+    except ValueError as e:
+        return {"success": False,
+                "message": "Error while getting tasks from last week"}, 400
