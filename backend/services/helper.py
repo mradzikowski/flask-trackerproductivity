4ac4086b32f28bd2bc9d52b94670088fb745da2d
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-04-06 23:44:54 +0100

    Add helper methods
    Add methods for calculating duration of task, getting user and getting last week's tasks

--- backend/services/helper.py
+++ backend/services/helper.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from backend.models.user import User, user_schema
 
 
@@ -37,3 +39,32 @@ def get_all_tasks_activity(user, is_active):
             if not task['is_active']:
                 finished_tasks.append(task)
         return finished_tasks
+
+
+def calculate_duration(task):
+    task.is_active = False
+    duration = datetime.now() - task.date_created
+    duration = divmod(duration.total_seconds(), 60)
+    duration_rounded_two_places = round(duration[0] / 60, 2)
+    task.duration = duration_rounded_two_places
+
+    return task
+
+
+def get_user(data_json):
+    if not data_json:
+        return {"success": False, "message": "No data provided."}
+    if not data_json['username']:
+        return {"success": False, "message": "No username provided."}
+    username = data_json['username']
+    return User.query.filter_by(username=username).first()
+
+
+def get_tasks_last_week(user):
+    time_diff = datetime.today() - timedelta(days=7)
+    tasks_user = user.tasks
+    tasks_last_week = []
+    for task in tasks_user:
+        if task.date_created >= time_diff:
+            tasks_last_week.append(task)
+    return tasks_last_week
